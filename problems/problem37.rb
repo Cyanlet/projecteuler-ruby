625dea1d20d71406fc7bebdc8488ed524fa32883
Dovy <djasulaitis05@gmail.com>
2021-09-17 16:37:48 +0200

    Update

--- problems/problem37.rb
+++ problems/problem37.rb
@@ -2,7 +2,7 @@ require 'prime'
 a = []
 Prime.each(1000000) do |p|
 	for i in 1..p.digits.count
-		break unless Prime.prime?(p.to_s.chars.shift(i).join.to_i) and Prime.prime?(p.to_s.chars.pop(i).join.to_i)
+		break unless p.to_s.chars.shift(i).join.to_i.prime? and p.to_s.chars.pop(i).join.to_i.prime?
 		a << p if i == p.to_s.chars.count
 	end
 end
