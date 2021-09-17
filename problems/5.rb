a7905cced924d772f797bf6a9434d5865c9492b9
Dovy <djasulaitis05@gmail.com>
2021-09-17 19:21:00 +0200

    Update for efficiency

--- problems/5.rb
+++ problems/5.rb
@@ -1,13 +1,1 @@
-# ugly brute force :(
-n = 1
-a = (1..20).to_a
-while true
-	b = false
-	a.each{|i|
-		break if n % i != 0
-		b = true if i == a.length
-	}
-	break if b
-	n += 1
-end
-puts n
+puts (1..20).inject(1) {|r, n| r.lcm n}
