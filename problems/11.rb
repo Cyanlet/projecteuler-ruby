68f49baa389b5f7ec3cc53040043582eb35b4137
Dovy <djasulaitis05@gmail.com>
2021-11-19 19:29:36 +0100

    Add Problem 11

--- problems/11.rb
+++ problems/11.rb
@@ -6,26 +6,26 @@ matrix = rows.map{|x|x[0].split.map(&:to_i)}
 products = []
 # horizontal
 (0..count-1).each{|y|
-    (0..count-nums).each{|x|
-        products << matrix[y][x..x+nums-1].reduce(:*)
-    }
+	(0..count-nums).each{|x|
+		products << matrix[y][x..x+nums-1].reduce(:*)
+	}
 }
 # vertical
 (0..count-nums).each{|y|
-    (0..count-1).each{|x|
-        products << (0..nums-1).inject(1){|n,i|n*matrix[y+i][x]}
-    }
+	(0..count-1).each{|x|
+		products << (0..nums-1).inject(1){|n,i|n*matrix[y+i][x]}
+	}
 }
 # diagonal to right
 (0..count-nums).each{|y|
-    (0..count-nums).each{|x|
-        products << (0..nums-1).inject(1){|n,i|n*matrix[y+i][x+i]}
-    }
+	(0..count-nums).each{|x|
+		products << (0..nums-1).inject(1){|n,i|n*matrix[y+i][x+i]}
+	}
 }
 # diagonal to left
 (0..count-nums).each{|y|
-    (nums-1..count-1).each{|x|
-        products << (0..nums-1).inject(1){|n,i|n*matrix[y+i][x-i]}
-    }
+	(nums-1..count-1).each{|x|
+		products << (0..nums-1).inject(1){|n,i|n*matrix[y+i][x-i]}
+	}
 }
 puts products.max
