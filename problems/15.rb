99e84172d400438a2f1f8fa446f522c06cdb89d5
Dovy <djasulaitis05@gmail.com>
2022-03-28 19:08:14 +0200

    Add comments for problem 15

--- problems/15.rb
+++ problems/15.rb
@@ -1,9 +1,8 @@
 size = 20
-# +1 because one row and column needs to be 1 to show only 1 path in each cell
+# +1 on size because one row and column needs to be 1 to show only 1 path in each cell at the edges of matrix, each cell initialized at 1
 a = Array.new(size+1){Array.new(size+1)}.map{|x|x.map{|y|y=1}}
-x = 1
 y = 1
-# dynamic programming, use previous computed cells to accumulate
+# use dynamic programming; previous computed cells -1 x and -1 y to accumulate and build up matrix
 1.upto(size).each{|i|
     1.upto(size).each{|j|   
         a[y][j] = a[y-1][j]+a[y][j-1]
